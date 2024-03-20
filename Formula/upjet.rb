b3063e6e66eebd8586a89eab4d18b3e797b40465
David Chevallier <david@chevallier.in>
2024-03-20 12:16:29 +0100

    fix sha256 for upjet

--- Formula/upjet.rb
+++ Formula/upjet.rb
@@ -2,7 +2,7 @@ class Upjet < Formula
     desc "Upjet: A code generation framework and runtime for Crossplane providers"
     homepage "https://github.com/crossplane/upjet"
     url "https://github.com/crossplane/upjet/archive/refs/tags/v1.1.0.tar.gz"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    sha256 "c9d40ddb9a7bcff084210d25ffc5381b1e8ec01b711f323568b48295691fda4f"
     depends_on "go" => :build
 
     def install
